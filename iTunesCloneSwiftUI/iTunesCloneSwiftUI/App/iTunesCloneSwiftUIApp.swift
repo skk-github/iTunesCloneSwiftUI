4311c9e4efda5bb1c92ede35721a1830c741293a
SK <2mailskk@gmail.com>
2023-07-03 12:40:48 +0530

    initial project creation
    adding basic folder structure and project initialization

---
+++ iTunesCloneSwiftUI/iTunesCloneSwiftUI/App/iTunesCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesCloneSwiftUIApp.swift
+//  iTunesCloneSwiftUI
+//
+//  Created by Krishna on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iTunesCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ iTunesCloneSwiftUI/iTunesCloneSwiftUI/Core/View/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iTunesCloneSwiftUI
+//
+//  Created by Krishna on 03/07/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
