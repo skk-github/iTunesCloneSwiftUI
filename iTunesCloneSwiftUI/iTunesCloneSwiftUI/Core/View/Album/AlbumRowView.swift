656c500f76a71e6613ea9c1e1edc2055acc3fde9
SK <2mailskk@gmail.com>
2023-07-05 14:36:37 +0530

    adding async image

--- iTunesCloneSwiftUI/iTunesCloneSwiftUI/Core/View/Album/AlbumRowView.swift
+++ iTunesCloneSwiftUI/iTunesCloneSwiftUI/Core/View/Album/AlbumRowView.swift
@@ -13,10 +13,40 @@ struct AlbumRowView: View {
     
     var body: some View {
         HStack{
+   
+            AsyncImage(url: URL(string: "")) { image in
+                image.frame(width:100, height: 100)
+                    
+            } placeholder: {
+                Image(systemName: "mountain.2.fill")
+                    .frame(width:100, height: 100)
+                    .background(Color.gray)
+            }
+
             VStack{
                 Text(albumItem.collectionName ?? "")
+                
             }
+            Spacer()
         }
+    }
+    
+    func getImage() -> UIImage {
+        guard let imageURL = URL(string: albumItem.artworkUrl100 ?? "") else {
+            return UIImage()
+        }
+        
+        Task{
+            do{
+                let (data, _) = try await URLSession.shared.data(from: imageURL)
+                return UIImage(data: data) ?? UIImage()
+            }catch{
+                return UIImage()
+            }
+            
+        
+        }
+        return UIImage()
     }
 }
 
